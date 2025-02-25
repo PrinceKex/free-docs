70706a861aeee6b01d137059b687fb4cb2783217
PrinceKex <ehigiator.ukeke@gmail.com>
2025-02-25 22:18:41 +0100

    headings, list, tables and images extension for tiptap added

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -1,6 +1,14 @@
 'use client'
 import { useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
+import TaskItem from '@tiptap/extension-task-item'
+import TaskList from '@tiptap/extension-task-list'
+import Table from '@tiptap/extension-table'
+import TableCell from '@tiptap/extension-table-cell'
+import TableHeader from '@tiptap/extension-table-header'
+import TableRow from '@tiptap/extension-table-row'
+import Image from '@tiptap/extension-image'
+import ImageResize from 'tiptap-extension-resize-image'
 
 export const Editor = () => {
  const editor = useEditor({
@@ -11,7 +19,17 @@ export const Editor = () => {
      'focus:outline-none print: border-0 bg-white border border-[#c7c7c7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text',
    },
   },
-  extensions: [StarterKit],
+  extensions: [
+   StarterKit,
+   Table,
+   TableRow,
+   TableHeader,
+   TableCell,
+   Image,
+   ImageResize,
+   TaskItem.configure({ nested: true }),
+   TaskList,
+  ],
   content: '<p>Hello World! 🌎️</p>',
  })
 
