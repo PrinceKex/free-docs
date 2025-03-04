9371487b3f358061bede052b854e4a0c032c2f41
PrinceKex <ehigiator.ukeke@gmail.com>
2025-03-04 23:19:23 +0100

    convex db schema done and db connected successfully

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,18 @@
+'use client'
 import Link from 'next/link'
 import React from 'react'
 import { Navbar } from './navbar'
 import { TemplatesGallery } from './templates-gallery'
+import { useQuery } from 'convex/react'
+import { api } from '../../../convex/_generated/api'
 
 const Home = () => {
+ const documents = useQuery(api.documents.get)
+
+ if (documents === undefined) {
+  return <p>Loading...</p>
+ }
+
  return (
   <div className='flex min-h-screen flex-col '>
    <div className='fixed top-0 left-0 right-0 z-10 h-16 bg-white o-4'>
@@ -11,6 +20,9 @@ const Home = () => {
    </div>
    <div className='mt-16'>
     <TemplatesGallery />
+    {documents?.map((document) => (
+     <span key={document._id}>{document.title}</span>
+    ))}
    </div>
   </div>
  )
