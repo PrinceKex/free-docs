9a1fb4ab658e0addedde7a8eea1571daf8fbd782
PrinceKex <ehigiator.ukeke@gmail.com>
2025-03-05 00:30:19 +0100

    template gallery function updated to access db

--- src/app/(home)/templates-gallery.tsx
+++ src/app/(home)/templates-gallery.tsx
@@ -8,10 +8,27 @@ import {
 } from '@/components/ui/carousel'
 import { templates } from '@/constants/templates'
 import { cn } from '@/lib/utils'
-import React from 'react'
+import { useMutation } from 'convex/react'
+import { useRouter } from 'next/navigation'
+import React, { useState } from 'react'
+import { api } from '../../../convex/_generated/api'
 
 export const TemplatesGallery = () => {
- const isCreating = false
+ const router = useRouter()
+ const create = useMutation(api.documents.create)
+ const [isCreating, setIsCreating] = useState(false)
+
+ const onTemplateClick = (title: string, initialContent: string) => {
+  setIsCreating(true)
+  create({ title, initialContent })
+   .then((documentId) => {
+    router.push(`/documents/${documentId}`)
+    //setIsCreating(false)
+   })
+   .finally(() => {
+    setIsCreating(false)
+   })
+ }
  return (
   <div className='bg-[#f1f3f4]'>
    <div className='max-w-screen-xl mx-auto px-16 py-6 flex flex-col gap-y-4'>
@@ -31,7 +48,7 @@ export const TemplatesGallery = () => {
         >
          <button
           disabled={isCreating}
-          onClick={() => {}}
+          onClick={() => onTemplateClick(template.label, '')}
           style={{
            backgroundImage: `url(${template.imageUrl})`,
            backgroundSize: 'cover',
